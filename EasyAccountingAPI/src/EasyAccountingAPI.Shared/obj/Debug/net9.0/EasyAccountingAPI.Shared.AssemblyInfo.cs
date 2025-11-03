dddb16ddc52ffbcf508d1f3a6b97367913b9ae7b
Sunil\Sunil Karmakar <sunil.karmakar@dslgroup.org>
2025-11-03 17:29:39 +0600

    Update Swagger file code.

--- EasyAccountingAPI/src/EasyAccountingAPI.Shared/obj/Debug/net9.0/EasyAccountingAPI.Shared.AssemblyInfo.cs
+++ EasyAccountingAPI/src/EasyAccountingAPI.Shared/obj/Debug/net9.0/EasyAccountingAPI.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyAccountingAPI.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5d50104088cb2b10489e2493955ce722e8511b43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7990c89076457c75cd886b7b7fae8a00e8d7d0e1")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyAccountingAPI.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyAccountingAPI.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ EasyAccountingAPI/src/EasyAccountingAPI/Controllers/Global/TestController.cs
@@ -0,0 +1,12 @@
+﻿namespace EasyAccountingAPI.Controllers.Global
+{
+    public class TestController : BaseController
+    {
+        [HttpGet]
+        [ProducesResponseType(typeof(bool), StatusCodes.Status200OK)]
+        public async Task<ActionResult<bool>> GetAllAsync()
+        {
+            return Ok(true);
+        }
+    }
+}
