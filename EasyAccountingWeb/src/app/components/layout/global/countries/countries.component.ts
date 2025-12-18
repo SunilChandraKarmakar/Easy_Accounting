f17e53af303ce251e456bfe391ad1bf7999ecc45
DESKTOP-4QSI4PI\Sunil Karmakar <sunil_karmakar@ymail.com>
2025-12-18 22:39:47 +0600

    Working on the client side pagination for country.

--- EasyAccountingWeb/src/app/components/layout/global/countries/countries.component.ts
+++ EasyAccountingWeb/src/app/components/layout/global/countries/countries.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzTableModule } from 'ng-zorro-antd/table';
@@ -6,30 +6,71 @@ import { RouterLink } from "@angular/router";
 import { NgxSpinnerModule, NgxSpinnerService } from "ngx-spinner";
 import { CountryGridModel, CountryService, FilterPageModel, FilterPageResultModelOfCountryGridModel } from '../../../../../api/base-api';
 import { ToastrService } from 'ngx-toastr';
+import {MatPaginator, MatPaginatorModule, PageEvent} from '@angular/material/paginator';
+import {MatTableDataSource, MatTableModule} from '@angular/material/table';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import {MatSort, MatSortModule, Sort} from '@angular/material/sort';
 
 @Component({
   selector: 'app-countries',
   templateUrl: './countries.component.html',
   styleUrls: ['./countries.component.css'],
   standalone: true,
-  imports: [NzButtonModule, NzDividerModule, NzTableModule, RouterLink, NgxSpinnerModule],
+  imports: [NzButtonModule, NzDividerModule, NzTableModule, RouterLink, NgxSpinnerModule, MatFormFieldModule, MatInputModule, MatTableModule, 
+    MatSortModule, MatPaginatorModule],
   providers: [CountryService]
 })
 
-export class CountriesComponent implements OnInit {
+export class CountriesComponent implements OnInit, AfterViewInit {
 
-  // Filter page result model
+  // Table property
   countries: CountryGridModel[] = [];
-  filterPageModel: FilterPageModel = new FilterPageModel();
+  displayedColumns: string[] = ['name', 'code', 'icon'];
+  dataSource: MatTableDataSource<CountryGridModel>;
   totalRecord: number = 0;
 
-  constructor(private countryService: CountryService, private spinnerService: NgxSpinnerService, private toastrService: ToastrService) { }
+  @ViewChild(MatPaginator) paginator!: MatPaginator;
+  @ViewChild(MatSort) sort!: MatSort;
 
-  ngOnInit() {    
-    // Initialize filter page model
-    this.initializeFilterPageModel();
+  // Filter page model
+  filterPageModel: FilterPageModel = new FilterPageModel();
 
+  constructor(private countryService: CountryService, private spinnerService: NgxSpinnerService, private toastrService: ToastrService) {
+    
+    // Call initial data source
+    this.dataSource = new MatTableDataSource(this.countries);
+  }
+
+  ngOnInit() {
+    // Initialize page filter model
+    this.initializeFilterModel();
+
+    // Get countries
     this.getCountries();
+  }
+
+  ngAfterViewInit() {
+    this.dataSource.paginator = this.paginator;
+    this.dataSource.sort = this.sort;
+  }
+
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.filterPageModel.filterValue = filterValue;
+    this.filterPageModel.pageIndex = 0;
+
+    // Get countries
+    this.getCountries();
+  }
+
+  // Initialize filter model
+  private initializeFilterModel(): void {
+    this.filterPageModel.pageIndex = 0;
+    this.filterPageModel.pageSize = 5;
+    this.filterPageModel.sortColumn = "name";
+    this.filterPageModel.sortOrder = "asc"
+    this.filterPageModel.filterValue = "";
   }
 
   // Get countries
@@ -37,22 +78,34 @@ export class CountriesComponent implements OnInit {
     this.spinnerService.show();
     this.countryService.getFilterCountries(this.filterPageModel).subscribe((result: FilterPageResultModelOfCountryGridModel) => {
       this.countries = result.items || [];
+      this.totalRecord = result.totalCount || 0;
+      this.dataSource = new MatTableDataSource(this.countries);
       this.spinnerService.hide();
       return;
     },
     (error: any) => {
       this.spinnerService.hide();
-      this.toastrService.error("Country cannot load at this time! Please, try again.", "Error");
+      this.toastrService.error("Country list is not show at this time! Please, try again.", "Error");
       return;
-    })
+    });
   }
 
-  // Initialize filter page model
-  private initializeFilterPageModel(): void {
+  // On page change
+  onPageChange(event: PageEvent): void {
+    this.filterPageModel.pageIndex = event.pageIndex;
+    this.filterPageModel.pageSize = event.pageSize;
+
+    // Get countries
+    this.getCountries();
+  }
+
+  // On sort change
+  onSortChange(event: Sort): void {
+    this.filterPageModel.sortColumn = event.active;
+    this.filterPageModel.sortOrder = event.direction.toString();
     this.filterPageModel.pageIndex = 0;
-    this.filterPageModel.pageSize = 10;
-    this.filterPageModel.sortColumn = "name";
-    this.filterPageModel.sortOrder = "asc";
-    this.filterPageModel.filterValue = "";
+    
+    // Get countries
+    this.getCountries();
   }
 }
