980ca00288c6bc493c4553f87030a6d1033da8bc
DESKTOP-4QSI4PI\Sunil Karmakar <sunil_karmakar@ymail.com>
2026-03-11 21:05:32 +0600

    Working on the employee list component.

--- EasyAccountingAPI/src/EasyAccountingAPI.Shared/obj/Debug/net9.0/EasyAccountingAPI.Shared.AssemblyInfo.cs
+++ EasyAccountingAPI/src/EasyAccountingAPI.Shared/obj/Debug/net9.0/EasyAccountingAPI.Shared.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EasyAccountingAPI.Shared")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3d56899f4410cda9fb1527268f5004f42773b867")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a8d7a20a8866958d3100f34f5e03cb91ae44c9b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EasyAccountingAPI.Shared")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EasyAccountingAPI.Shared")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
